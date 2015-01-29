91c6ff736e515a9494ead9ee87c86864d483731e
Patrick Werner <pwerner2@gmail.com>
2015-01-29 16:00:28 -0500

    first draft, main.js.

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,30 @@
+(function(){
 
+	$(document).ready(function(){
+		$('.submitmsg').click(function(){
+			var 
 
 $.ajax({
-	url: "http://tiny-pizza-server.herokuapp.com/collections/greenville-chats",
-	type: "POST",
-	data: {
-		message:
-		username:
-		createdAt:
-	}
+	url: 'http://tiny-pizza-server.herokuapp.com/collections/greenville-chats',
+	type: 'GET',
+	datatype: 'jsonp',
+
+	});
+
+$.ajax({
+	url: 'http://tiny-pizza-server.herokuapp.com/collections/greenville-chats',
+	type: 'POST',
+	datatype: 'jsonp',
+
+	});
+
+function renderTemplate(name, data) {
+	var $template = $('[data-template-name=' + name + ']').text();
+		$.each(data, function(prop, value) {
+			$template = $template.replace('<% ' + data + ' %>', value);
+		});
+
+		});
+	});
 });
-
-
+});
