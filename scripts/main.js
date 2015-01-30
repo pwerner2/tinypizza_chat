eb535624f82988a4dc49fba3c039c1ad41ca39a0
Patrick Werner <pwerner2@gmail.com>
2015-01-30 14:01:58 -0500

    js message send/recv written

--- scripts/main.js
+++ scripts/main.js
@@ -45,39 +45,68 @@
 	$(document).ready(function() {
 
 
-		var $url = "http://tiny-pizza-server.herokuapp.com/collections/greenville-chats";
-		var $user;
-		var $message;
+		var servUrl = "http://tiny-pizza-server.herokuapp.com/collections/greenville-chats";
+		
+		var userName = prompt("Username?");
 
-		$user = "Sinatra";
-		$message = "DOOBIE DOOBIE DOOOOOOOOOO";
-		// $createdAt = date.
+		if (username === null){
+			userName = "Anonymous";
+		}
 
-		//
-		// Get All Chat Messages From Server
-		//
-		$.ajax({
-			url: $url,
-			type: "GET"
-		}).done(function(data) {
-			console.log(data);
+		var renderMessageTemplate = _.template($('.message-data').text());
+		var messageTemplate = $('.messages-list');
 
-		});
+		$('.send-button').on('click', sendMessage);
 
-		$.ajax({
-			url: "http://tiny-pizza-server.herokuapp.com/collections/greenville-chats",
-			type: "POST",
-			data: {
-				message: "I don't have to drink my urine, but it's sterile and I like the taste",
-				username: "That guy from waterworld",
-				createdAt: Date.now()
-			}
-		}).done(function(data) {
-			console.log(data);
+		function storeUsername() {
+			userName = $('.username-field').val();
+			console.log(userName);
+		}
 
-		});
+		function sendMessage() {
+			var timeStamp = moment().format();
+			var newMessage = $('.text-box').val();
+			$.ajax({
+				url: servUrl,
+				type: "POST",
+				data: {
+					username: userName,
+					message: newMessage,
+					createdAt: timeStamp
+				}
+			}).done(function(data) {
+				console.log(data);
+			});
+		}
 
+		// message retrieval
+		
+	function getMsg() {
+	messageTemplate.empty();
+	$.ajax({
+		url: servUrl,
+		type: "GET"
+	}).done(function(messages) {
+	
+	messages = messages.reverse();
+	_.each(messages, function(message) {
 
+		if (message.created.At == null) {
+			message.createdAt = "unknown";
+		} else {
+			message.createdAt = moment(message.createdAt).fromNow();
+		}
 
+		if (message.message == null) {
+			message.message = '';
+		}
+		
+		if (message.username == null) {
+			message.username = '';
+		}
+
+		messageTemplate.append(renderMessageTemplate(message));
+		$('messages-container').scrollTop($'.messages-container').prop('scrollHeight'));
 	});
-})();
+	});
+});
