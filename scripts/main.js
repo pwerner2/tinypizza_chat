6832d569991396ad5dbd96319d4f7b71d8f06964
Weston Humphreys <braverun@gmail.com>
2015-02-01 18:09:29 -0500

    fixed JS conficts between WH-Code and master

--- scripts/main.js
+++ scripts/main.js
@@ -1,47 +1,14 @@
 (function() {
 	'use strict';
 
-<<<<<<< HEAD
-
-var userName = "";
-var messageContent = "";
-var currentTime = "";
-var chatTemplate = "";
-
-=======
->>>>>>> WH-Code
 var servUrl = "http://tiny-pizza-server.herokuapp.com/collections/greenville-chats";
 var messageboardTemplate = _.template($('[data-template-name=message-post]').text());
 var $messageContainer = $('.messageContainer');
 
-<<<<<<< HEAD
-
-$(document).ready(function() {
-
-	currentTime = Date.now();
 
 
+//Template for Displaying Posts
 
-
-$('#logButton').on('click', function(){
-	event.preventDefault();
-	if($(".userName-value").val() === '') {
-	prompt("Enter an username");
-	} else {
-	username = $(".userName-value").val();
-	console.log(userName);
-	$('.userName-stored').text(userName);
-	$('.message-reply').removeClass('hidden');
-	$('.messageContainer').scrollTop($('.messageContainer')[0].scrollHeight);
-	$('.messageContainer').removeClass('hidden');
-	$('messageContainer userName').addClass('hidden');
-     }
-
-   });
-});
-
-=======
->>>>>>> WH-Code
 $(document).ready(function() {
   $.ajax(servUrl).done(function(posts) {
     _.each(posts, function(post) {
@@ -50,35 +17,5 @@ $(document).ready(function() {
     });
   });
 });
-<<<<<<< HEAD
 
-
-$('.submit').click(function() {
-    username = ("#submit").value();
-    console.log('username');
-  });
-setInterval(messageboardTemplate, 7000);
-
-  $('.send-button').on('click', function(){
-    event.preventDefault();
-    if($("#input-field").val() === ''){
-      alert("pizza bell taco hut");
-    } else {
-      messageContent = $("#input-field").val();
-      console.log(messageContent);
-      $.ajax({
-        url: servUrl,
-        type: "POST",
-        data: {
-          message: messageContent,
-          username: username,
-          createdAt: Date.now()
-        }
-      });
-    }
-    $('#input-field').val('');
-  });
-
-=======
->>>>>>> WH-Code
 })();
