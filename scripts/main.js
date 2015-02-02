01702711a522e03624052fca2e85c0882fadc7a3
Patrick Werner <pwerner2@gmail.com>
2015-02-02 13:40:42 -0500

    this code is *almost* working, but automatic population of list is not working

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,71 @@
+var serverUrl = "http://tiny-pizza-server.herokuapp.com/collections/greenville-chats";
+var messageboardTemplate = _.template($('[data-template-name=message-post]').text());
+var $messageContainer = $('.messageContainer');
+var msgcontent = "";
+var username = "";
+var currentTime = "";
+var $msgLog = "";
+
 (function() {
 	'use strict';
 
-var servUrl = "http://tiny-pizza-server.herokuapp.com/collections/greenville-chats";
-var messageboardTemplate = _.template($('[data-template-name=message-post]').text());
-var $messageContainer = $('.messageContainer');
 
 //this is the part that pulls and displays msgs from server. Vars inconsistent w/pbranch
 
 $(document).ready(function() {
-  $.ajax(servUrl).done(function(posts) {
+	
+	currentTime = Date.now();
+
+	username = prompt("please enter a username!");
+
+  $.ajax(serverUrl).done(function(posts) {
     _.each(posts, function(post) {
       $messageContainer.append(messageboardTemplate(post));
 			console.log(posts);
     });
   });
 });
+
+// This is the bit that makes posting work
+
+$('#send-button').on('click', function() {
+	event.preventDefault();
+	if($('.character-input').val() === ''){
+		alert("Invalid input, try again");
+	} else {
+		msgcontent = $('.character-input').val();
+		console.log(msgcontent);
+
+		$.ajax({
+			url: serverUrl,
+			type: "POST",
+			data: {
+				message: msgcontent,
+				username: username,
+				createdAt: Date.now()
+			}
+		});
+	}
+
+		$('.character-input').val('');
+	});
 })();
+
+	function getData() {
+		
+		$.ajax(serverUrl).done(chatFilter);
+	}
+
+	function chatFilter(chatData) {
+		var filteredData = _.filter(chatData, function(parseData) {
+			return parseData.createdAt >= currentTime;
+		});
+
+		_.each(filteredData, function(printData) {
+			$msgLog.append($message-post(printData));
+			$('.message-reply').scrollTop($("messageboardTemplate")[0].scrollHeight);
+		});
+	}
+
+setInterval(getData, 3000);
+
